131e189bca43b6523537cc5c2316a484e8cfc178
Lemniscate <Lemniscate_zero@proton.me>
2024-10-10 17:05:10 +0000

    Window Added, Menu Added

--- Window.py
+++ Window.py
@@ -1,11 +1,44 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
 
 class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
     self.setWindowTitle("Biomimetic Design Tool")
     self.setGeometry(100, 100, 800, 600)
+
+    self.create_menu_bar()
+  def create_menu_bar(self):
+
+    menu_bar = self.menuBar()
+    file_menu = menu_bar.addMenu("File")
+
+    open_action = QAction("Open", self)
+    open_action.triggered.connect(self.open_file_dialog)
+
+    save_action = QAction("Save", self)
+    save_action.triggered.connect(self.save_file_dialog)
+
+    exit_action = QAction("Exit", self)
+    exit_action.triggered.connect(self.close)
+
+    #Add Actions To Menu
+    file_menu.addAction(open_action)
+    file_menu.addAction(save_action)
+    file_menu.addAction(exit_action)
+
+  def open_file_dialog(self):
+    options = QFileDialog.Options()
+    file_name , _ = QFileDialog.getOpenFileName(self, "Open Design File", "", "All Files (*);;Design Files (*.des)", options=options)
+    if file_name:
+      print(f"Opening file: {file_name}")
+
+  def save_file_dialog(self):
+    options = QFileDialog.Options()
+    file_name , _ = QFileDialog.getSaveFileName(self, "Save Design File", "", "All Files (*);;Design Files (* .des)", options=options)
+    if file_name:
+      print(f"Saving file: {file_name}")
+
 
 #initialization of the application
 app = QApplication(sys.argv)
